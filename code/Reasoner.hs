8d9e6758639accc61b3f34f84c266682678f6525
Bryan St. Amour <bryan.stamour@gmail.com>
2013-12-20 21:33:01 -0500

    Core SL opinion types added. Also binomial addition and subtraction.

--- code/Reasoner.hs
+++ code/Reasoner.hs
@@ -1,6 +1,5 @@
 
 
-
 {- | This module represents the core reasoner architecture: reasoners, frames,
      belief mass assignments, and their respective instance declarations.
 
@@ -16,6 +15,7 @@ module Reasoner where
 
 import Data.Maybe
 import Control.Applicative
+import Control.Monad
 import qualified Data.Map as M
 
 
@@ -77,9 +77,134 @@ instance Applicative (Reasoner h f) where
 
 -- | Access the belief mass that we are towing around.
 getMass :: (Ord h, Ord f) => h -> Reasoner h f (MassMap f)
-getMass holder = Reasoner $ \ (MassAssignment m) -> fromMaybe emptyMass (M.lookup holder m)
+getMass holder = Reasoner $ \ (MassAssignment m) ->
+  fromMaybe emptyMass (M.lookup holder m)
 
 
 -- | In the absense of any mass, we assume total uncertainty.
 emptyMass :: Ord f => MassMap f
 emptyMass = MassMap $ M.fromList [(Theta, 1)]
+
+
+{- SL-related code.
+
+   Below we define hyper opinions first, as they're the most general form
+   of subjective opinion. Afterward we define multinomial opinions and methods
+   to (possibly) generate a multinomial from a hyper. Finally the last, and
+   most interesting class, is the binomial opinion.
+-}
+
+
+-- | The most general form of opinion in Subjective Logic.
+data HyperOpinion f =
+  HyperOpinion
+  { hyperBelief      :: M.Map [f] Rational
+  , hyperUncertainty :: Rational
+  , hyperBaseRate    :: M.Map f Rational
+  }
+
+
+-- | Construct a hyper opinion from the belief mass. This must be constrained
+--   to within the reasoner because it requires direct access to the belief mass.
+opinion :: (Ord h, Ord f) => h -> Reasoner h f (HyperOpinion f)
+opinion holder = go <$> getMass holder
+  where
+    go (MassMap mass) = let u = fromMaybe 0 (M.lookup Theta mass)
+                            b = M.mapKeys (\(Subset s) -> s) $ M.delete Theta mass
+                            a = M.empty -- TODO
+                        in HyperOpinion b u a
+
+
+-- | Not as general as a hyper opinion.
+data MultinomialOpinion f =
+  MultinomialOpinion
+  { multiBelief      :: M.Map f Rational
+  , multiUncertainty :: Rational
+  , multiBaseRate    :: M.Map f Rational
+  }
+
+
+isMultinomial :: HyperOpinion f -> Bool
+isMultinomial = M.foldrWithKey (\k _ x -> x && length k == 1) True . hyperBelief
+
+
+-- | If the given hyper opinion is actually multinomial, then return it,
+--   else return Nothing.
+multinomial :: Ord f => HyperOpinion f -> Maybe (MultinomialOpinion f)
+multinomial hyper = do guard (isMultinomial hyper)
+                       let b = M.mapKeys (\k -> head k) $ hyperBelief hyper
+                       let u = hyperUncertainty hyper
+                       let a = hyperBaseRate hyper
+                       return $ MultinomialOpinion b u a
+
+
+-- | A binomial opinion is either a binary frame, or a binary partitioning
+--   of a multinomial frame.
+data BinomialOpinion f =
+  BinomialOpinion
+  { binBelief      :: Rational
+  , binDisbelief   :: Rational
+  , binUncertainty :: Rational
+  , binBaseRate    :: Rational
+  }
+
+
+isBinomial :: MultinomialOpinion f -> Bool
+isBinomial = (2 ==) . M.size . multiBelief
+
+
+-- | Create a binomial opinion from a multinomial opinion if the multinomial
+--   is actually a binomial opinion (frame has cardinality 2). Else, return
+--   Nothing.
+binomial :: Ord f => f -> MultinomialOpinion f -> Maybe (BinomialOpinion f)
+binomial f multi = do guard (isBinomial multi)
+                      b <- M.lookup f (multiBelief multi)
+                      a <- M.lookup f (multiBaseRate multi)
+                      let u = multiUncertainty multi
+                      let d = 1 - b - u
+                      return $ BinomialOpinion b d u a
+
+
+-- | Coarsen a multinomial opinion down into a binomial opinion. This creates
+--   a binomial opinion over a binary partitioning of the frame.
+coarsen :: [f] -> MultinomialOpinion f -> BinomialOpinion f
+coarsen subset multi = undefined
+
+
+{- Subjective Logic operators -}
+
+
+binomialSum :: BinomialOpinion f -> BinomialOpinion f -> BinomialOpinion f
+binomialSum op1 op2 =
+  let b = bx + by
+      d = (ax * (dx - by) + ay * (dy - bx)) / (ax + ay)
+      u = (ax * ux + ay * uy) / (ax + ay)
+      a = ax + ay
+  in BinomialOpinion b d u a
+  where
+    bx = binBelief op1
+    by = binBelief op2
+    dx = binDisbelief op1
+    dy = binDisbelief op2
+    ux = binUncertainty op1
+    uy = binUncertainty op2
+    ax = binBaseRate op1
+    ay = binBaseRate op2
+
+
+binomialDiff :: BinomialOpinion f -> BinomialOpinion f -> BinomialOpinion f
+binomialDiff op1 op2 =
+  let b = bx - by
+      d = (ax * (dx + by) - ay * (1 + by - bx - uy)) / (ax - ay)
+      u = (ax * ux - ay * uy) / (ax - ay)
+      a = ax - ay
+  in BinomialOpinion b d u a
+  where
+    bx = binBelief op1
+    by = binBelief op2
+    dx = binDisbelief op1
+    dy = binDisbelief op2
+    ux = binUncertainty op1
+    uy = binUncertainty op2
+    ax = binBaseRate op1
+    ay = binBaseRate op2
