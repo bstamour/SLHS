61e1580f1c281c5e8e3fb008b9079c38a06bf38c
Bryan St. Amour <bryan.stamour@gmail.com>
2014-02-12 18:34:50 -0500

    added a few operators to test out the type system.

--- code/SL.hs
+++ code/SL.hs
@@ -25,3 +25,44 @@ instance Monad SLValue where
 
   (Error s)   >>= _ = Error s
   (SLValue x) >>= f = f x
+
+
+data SLState = SLState -- TODO
+
+
+data SLExpr a = SLExpr { unSLExpr :: SLState -> a }
+
+
+instance Functor SLExpr where
+  fmap f sx = SLExpr $ \st -> f (unSLExpr sx st)
+
+
+instance Applicative SLExpr where
+  pure  = return
+  (<*>) = ap
+
+
+instance Monad SLExpr where
+  return x = SLExpr $ \_ -> x
+
+  sx >>= f = SLExpr $ \st -> let x  = unSLExpr sx st
+                                 sy = f x
+                             in unSLExpr sy st
+
+
+type SL a = SLExpr (SLValue a)
+
+
+runSL :: SL a -> SLState -> SLValue a
+runSL = unSLExpr
+
+
+data Opinion = Opinion deriving Show
+
+
+(<++>) :: SL Opinion -> SL Opinion -> SL Opinion
+op1 <++> op2  = pure $ Error "(<++>): Not yet implemented."
+
+
+(</\>) :: SL Opinion -> SL Opinion -> SL Opinion
+op1 </\> op2  = pure $ Error "(</\\>): Not yet implemented."
