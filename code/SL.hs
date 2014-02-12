16da3eef3088f217b77ea4efd2a23cbeedc11143
Bryan St. Amour <bryan.stamour@gmail.com>
2014-02-12 18:53:39 -0500

    More changes.

--- code/SL.hs
+++ code/SL.hs
@@ -5,6 +5,8 @@ import Control.Monad (ap)
 import Control.Applicative
 
 
+-- A simple wrapper type to store either a value or an error message.
+
 data SLValue a = Error String
                | SLValue a
                deriving (Eq, Show)
@@ -23,9 +25,11 @@ instance Applicative SLValue where
 instance Monad SLValue where
   return = SLValue
 
-  (Error s)   >>= _ = Error s
   (SLValue x) >>= f = f x
+  (Error s)   >>= _ = Error s
 
+
+-- A custom state monad for SL computations.
 
 data SLState = SLState -- TODO
 
@@ -45,10 +49,12 @@ instance Applicative SLExpr where
 instance Monad SLExpr where
   return x = SLExpr $ \_ -> x
 
-  sx >>= f = SLExpr $ \st -> let x  = unSLExpr sx st
-                                 sy = f x
+  sx >>= f = SLExpr $ \st -> let sy = f (unSLExpr sx st)
                              in unSLExpr sy st
 
+
+-- Type wrapper for easier-looking code. Maybe make this a newtype and
+-- wrap it properly.
 
 type SL a = SLExpr (SLValue a)
 
@@ -56,6 +62,23 @@ type SL a = SLExpr (SLValue a)
 runSL :: SL a -> SLState -> SLValue a
 runSL = unSLExpr
 
+
+-- Opinions and their operators.
+--
+-- TODO: Write all code in terms of hyper opinions. Specialize when applicable.
+--
+-- Write the operators to work internally directly on Opinion types, that way
+-- we can do tricks with short-circuit computation (thank you, SLValue).
+--
+-- We will offer make_opinion functions to construct opinions. They require
+-- access to the state in order to compute them. A typical operator may look
+-- like this
+--
+-- op1 <*> op2 = do op1' <- op1
+--                  op2' <- op2
+--                  return $ op_impl <$> op1' <*> op2'
+--   where
+--     op_impl :: Opinion -> Opinion -> Opinion
 
 data Opinion = Opinion deriving Show
 
