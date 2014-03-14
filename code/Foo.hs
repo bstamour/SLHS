12b566cc8c52a663f052a9dd6bb6001f519be4c2
Bryan St. Amour <bryan.stamour@gmail.com>
2014-03-13 20:57:43 -0400

    stuff

--- code/Foo.hs
+++ code/Foo.hs
@@ -1,19 +1,16 @@
 module Foo where
 
-
 import Data.Maybe
 import Control.Monad.State
 import qualified Data.Map as M
 import qualified Data.Set as S
-
 
 type Frame = S.Set
 type Subframe = S.Set
 type BeliefDistribution a = M.Map [a] Rational
 type BaseRateVector a = M.Map a Rational
 
-
-data BinomialOpinion a = BinomialOpinion
+data BinomialOpinion a = BinOp
                          a
                          a
                          Rational
@@ -21,224 +18,211 @@ data BinomialOpinion a = BinomialOpinion
                          Rational
                          Rational
 
+data Coarsened a = C (Subframe a) deriving (Eq, Ord)
 
-data Coarsened a = X (Subframe a)
-                 | NotX (Subframe a)
-                 deriving (Eq, Ord)
-
-
-data MultinomialOpinion a = MultinomialOpinion
+data MultinomialOpinion a = MultOp
                             (Frame a)
                             (M.Map a Rational)
                             Rational
                             (M.Map a Rational)
 
-
-data HyperOpinion a = HyperOpinion
+data HyperOpinion a = HyperOp
                       (Frame a)
                       (M.Map (Subframe a) Rational)
                       Rational
                       (M.Map a Rational)
 
-
 -----------------------------------------------------------------------------------------
 -- Constructors.
 -----------------------------------------------------------------------------------------
 
-
-makeBinomial :: BeliefDistribution a
+makeBinomial :: Frame a
+             -> BeliefDistribution a
              -> BaseRateVector a
              -> Maybe (BinomialOpinion a)
 makeBinomial = undefined
-
 
 makeMultinomial :: BeliefDistribution a
                 -> BaseRateVector a
                 -> Maybe (MultinomialOpinion a)
 makeMultinomial = undefined
 
-
 makeHyper :: BeliefDistribution a
           -> BaseRateVector a
           -> Maybe (HyperOpinion a)
 makeHyper = undefined
 
-
 -----------------------------------------------------------------------------------------
 -- Accessor functions.
 -----------------------------------------------------------------------------------------
 
-
-bBelief :: BinomialOpinion a -> Rational
-bBelief (BinomialOpinion _ _ b _ _ _) = b
-
-
-bDisbelief :: BinomialOpinion a -> Rational
-bDisbelief (BinomialOpinion _ _ _ d _ _) = d
-
-
-bUncertainty :: BinomialOpinion a -> Rational
-bUncertainty (BinomialOpinion _ _ _ _ u _) = u
-
-
-bBaseRate :: BinomialOpinion a -> Rational
-bBaseRate (BinomialOpinion _ _ _ _ _ a) = a
-
-
 class Multinomial m where
-  mBelief      :: Ord a => m a -> a -> Maybe Rational
+  mBelief :: Ord a => m a -> a -> Maybe Rational
   mUncertainty :: m a -> Rational
-  mBaseRate    :: Ord a => m a -> a -> Maybe Rational
-
+  mBaseRate :: Ord a => m a -> a -> Maybe Rational
 
 instance Multinomial BinomialOpinion where
-  mBelief (BinomialOpinion x y b d _ _) e
-    | e == x    = Just b
-    | e == y    = Just d
+  mBelief (BinOp x y b d _ _) e
+    | e == x = Just b
+    | e == y = Just d
     | otherwise = Nothing
 
-  mUncertainty (BinomialOpinion _ _ _ _ u _) = u
+  mUncertainty (BinOp _ _ _ _ u _) = u
 
-  mBaseRate (BinomialOpinion x y _ _ _ a) e
-    | e == x    = Just a
-    | e == y    = Just (1 - a)
+  mBaseRate (BinOp x y _ _ _ a) e
+    | e == x = Just a
+    | e == y = Just (1 - a)
     | otherwise = Nothing
-
 
 instance Multinomial MultinomialOpinion where
-  mBelief (MultinomialOpinion xs b _ _) x
+  mBelief (MultOp xs b _ _) x
     | x `S.member` xs = Just (fromMaybe 0 (M.lookup x b))
-    | otherwise       = Nothing
-
-  mUncertainty (MultinomialOpinion _ _ u _) = u
-
-  mBaseRate (MultinomialOpinion xs _ _ a) x
-    | x `S.member` xs = Just (fromMaybe 0 (M.lookup x a))
-    | otherwise       = Nothing
-
-
-class Hyper h where
-  hBelief      :: Ord a => h a -> Subframe a -> Maybe Rational
-  hUncertainty :: h a -> Rational
-  hBaseRate    :: Ord a => h a -> a -> Maybe Rational
-
-
-instance Hyper BinomialOpinion where
-  hBelief (BinomialOpinion x y b d _ _) e
-    | S.size e /= 1 = Nothing
-    | e' == x       = Just b
-    | e' == y       = Just d
-    | otherwise     = Nothing
-    where
-      [e'] = S.toList e
-
-  hUncertainty (BinomialOpinion _ _ _ _ u _) = u
-
-  hBaseRate (BinomialOpinion x y _ _ _ a) e
-    | e == x    = Just a
-    | e == y    = Just (1 - a)
     | otherwise = Nothing
 
+  mUncertainty (MultOp _ _ u _) = u
 
-instance Hyper MultinomialOpinion where
-  hBelief (MultinomialOpinion xs b _ _) e
-    | S.size e /= 1    = Nothing
-    | e' `S.member` xs = Just (fromMaybe 0 (M.lookup e' b))
-    | otherwise        = Nothing
+  mBaseRate (MultOp xs _ _ a) x
+    | x `S.member` xs = Just (fromMaybe 0 (M.lookup x a))
+    | otherwise = Nothing
+
+class Hyper h where
+  hBelief :: Ord a => h a -> Subframe a -> Maybe Rational
+  hUncertainty :: h a -> Rational
+  hBaseRate :: Ord a => h a -> a -> Maybe Rational
+
+instance Hyper BinomialOpinion where
+  hBelief (BinOp x y b d _ _) e
+    | S.size e /= 1 = Nothing
+    | e' == x = Just b
+    | e' == y = Just d
+    | otherwise = Nothing
     where
       [e'] = S.toList e
 
-  hUncertainty (MultinomialOpinion _ _ u _) = u
+  hUncertainty (BinOp _ _ _ _ u _) = u
 
-  hBaseRate (MultinomialOpinion xs _ _ a) x
+  hBaseRate (BinOp x y _ _ _ a) e
+    | e == x = Just a
+    | e == y = Just (1 - a)
+    | otherwise = Nothing
+
+instance Hyper MultinomialOpinion where
+  hBelief (MultOp xs b _ _) e
+    | S.size e /= 1 = Nothing
+    | e' `S.member` xs = Just (fromMaybe 0 (M.lookup e' b))
+    | otherwise = Nothing
+    where
+      [e'] = S.toList e
+
+  hUncertainty (MultOp _ _ u _) = u
+
+  hBaseRate (MultOp xs _ _ a) x
     | x `S.member` xs = Just (fromMaybe 0 (M.lookup x a))
-    | otherwise       = Nothing
-
+    | otherwise = Nothing
 
 instance Hyper HyperOpinion where
-  hBelief (HyperOpinion xs b _ _) es
+  hBelief (HyperOp xs b _ _) es
     | es `S.isSubsetOf` xs = Just (fromMaybe 0 (M.lookup es b))
-    | otherwise            = Nothing
+    | otherwise = Nothing
 
-  hUncertainty (HyperOpinion _ _ u _) = u
+  hUncertainty (HyperOp _ _ u _) = u
 
-  hBaseRate (HyperOpinion xs _ _ a) x
+  hBaseRate (HyperOp xs _ _ a) x
     | x `S.member` xs = Just (fromMaybe 0 (M.lookup x a))
-    | otherwise       = Nothing
-
+    | otherwise = Nothing
 
 -----------------------------------------------------------------------------------------
 -- Binomial operators.
 -----------------------------------------------------------------------------------------
 
-
-add :: BinomialOpinion (Coarsened a)
-    -> BinomialOpinion (Coarsened a)
-    -> Maybe (BinomialOpinion (Coarsened a))
-add = undefined
-
-
-subtract :: BinomialOpinion (Coarsened a)
-         -> BinomialOpinion (Coarsened a)
-         -> Maybe (BinomialOpinion (Coarsened a))
-subtract = undefined
-
-
-times :: BinomialOpinion (Coarsened a)
-      -> BinomialOpinion (Coarsened a)
-      -> Maybe (BinomialOpinion (Coarsened a))
-times = undefined
-
-
-divide :: BinomialOpinion (Coarsened a)
+add :: Ord a
+       => BinomialOpinion (Coarsened a)
        -> BinomialOpinion (Coarsened a)
        -> Maybe (BinomialOpinion (Coarsened a))
+add (BinOp (C xs1) (C ys1) b1 d1 u1 a1) (BinOp (C xs2) (C ys2) b2 d2 u2 a2)
+  | frm1 /= frm2 = Nothing
+  | not (S.null $ xs1 `S.intersection` ys1) = Nothing
+  | otherwise = Just $ BinOp (C newX) (C newY) b' d' u' a'
+  where
+    newX = xs1 `S.union` xs2
+    newY = frm1 `S.difference` newX
+
+    b' = b1 + b2
+    d' = (a1 * (d1 - b2) + a2 * (d2 - b1)) / (a1 + a2)
+    u' = (a1 * u1 + a2 * u2) / (a1 + a2)
+    a' = a1 + a2
+
+    frm1 = xs1 `S.union` ys1
+    frm2 = xs2 `S.union` ys2
+
+subtract :: Ord a
+            => BinomialOpinion (Coarsened a)
+            -> BinomialOpinion (Coarsened a)
+            -> Maybe (BinomialOpinion (Coarsened a))
+subtract (BinOp (C xs1) (C ys1) b1 d1 u1 a1) (BinOp (C xs2) (C ys2) b2 d2 u2 a2)
+  | frm1 /= frm2 = Nothing
+  | not (xs2 `S.isSubsetOf` xs1) = Nothing
+  | otherwise = Just $ BinOp (C newX) (C newY) b' d' u' a'
+  where
+    newX = xs1 `S.union` xs2
+    newY = frm1 `S.difference` newX
+
+    b' = b1 - b2
+    d' = (a1 * (d1 + b2) - a2 * (1 + b2 - b1 - u2)) / (a1 - a2)
+    u' = (a1 * u1 - a2 * u2) / (a1 - a2)
+    a' = a1 - a2
+
+    frm1 = xs1 `S.union` ys1
+    frm2 = xs2 `S.union` ys2
+
+times :: Ord a
+         => BinomialOpinion a
+         -> BinomialOpinion a
+         -> Maybe (BinomialOpinion a)
+times op1 op2 = undefined
+
+divide :: Ord a
+          => BinomialOpinion a
+          -> BinomialOpinion a
+          -> Maybe (BinomialOpinion a)
 divide = undefined
 
-
-cotimes :: BinomialOpinion (Coarsened a)
-        -> BinomialOpinion (Coarsened a)
-        -> Maybe (BinomialOpinion (Coarsened a))
+cotimes :: Ord a
+           => BinomialOpinion a
+           -> BinomialOpinion a
+           -> Maybe (BinomialOpinion a)
 cotimes = undefined
 
-
-codivide :: BinomialOpinion (Coarsened a)
-         -> BinomialOpinion (Coarsened a)
-         -> Maybe (BinomialOpinion (Coarsened a))
+codivide :: Ord a
+            => BinomialOpinion a
+            -> BinomialOpinion a
+            -> Maybe (BinomialOpinion a)
 codivide = undefined
-
 
 -----------------------------------------------------------------------------------------
 -- Multinomial operators.
 -----------------------------------------------------------------------------------------
 
-
-multiply :: Multinomial op => op a -> op a -> HyperOpinion a
+multiply :: Multinomial op => op a -> op b -> HyperOpinion (a, b)
 multiply = undefined
-
 
 -----------------------------------------------------------------------------------------
 -- Hyper operators.
 -----------------------------------------------------------------------------------------
 
-
 cfuse :: Hyper op => op a -> op a -> HyperOpinion a
 cfuse = undefined
 
-
 afuse :: Hyper op => op a -> op a -> HyperOpinion a
 afuse = undefined
-
 
 -----------------------------------------------------------------------------------------
 -- State.
 -----------------------------------------------------------------------------------------
 
-
 data SLData h a = SLData
                   { sldFrames :: [Frame a]
                   , sldOwners :: [h]
                   }
-
 
 type SLState h a t = State (SLData h a)
